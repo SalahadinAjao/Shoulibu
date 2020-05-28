5beee3ae4e93663300e3ea912a080d15dbe74d4f
houlintao <437547058@qq.com>
2020-05-28 18:44:16 +0800

    Commit

---
+++ Api/src/main/java/org/example/dao/ApiUserMapper.java
@@ -0,0 +1,22 @@
+package org.example.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.example.Entity.UserEntity;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: houlintao
+ * @Date:2020/5/28 下午6:32
+ * @email 437547058@qq.com
+ * @Version 1.0
+ */
+public interface ApiUserMapper extends BaseDao<UserEntity> {
+
+    UserEntity queryByMobile(String mobile);
+
+    UserEntity queryByOpenId(@Param("openId") String openId);
+
+
+}
