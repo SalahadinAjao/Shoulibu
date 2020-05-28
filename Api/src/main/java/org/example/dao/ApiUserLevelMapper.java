156d122694955e5d12b1b031a9cd8fef304dd23b
houlintao <437547058@qq.com>
2020-05-28 18:53:47 +0800

    Commit

---
+++ Api/src/main/java/org/example/dao/ApiUserLevelMapper.java
@@ -0,0 +1,13 @@
+package org.example.dao;
+
+import org.example.Entity.UserLevelEntity;
+
+/**
+ * @Author: houlintao
+ * @Date:2020/5/28 下午6:50
+ * @email 437547058@qq.com
+ * @Version 1.0
+ */
+public interface ApiUserLevelMapper extends BaseDao<UserLevelEntity> {
+
+}
